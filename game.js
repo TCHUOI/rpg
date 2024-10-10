ff3165f02fa8fc657e95b4ad44378d3385641e60
TCHUOI <tcui@bostonk12.org>
2024-10-10 10:40:11 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,7 @@
+let canvas = document.getElementById("canvi");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let ctx = canvas.getContext("2d");
 
+
+ctx.fillRect(0, 0, 100, 100);
