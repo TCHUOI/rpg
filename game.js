788d49b7c8c347418e0cd0f97a46214b34b716f4
TCHUOI <tcui@bostonk12.org>
2024-10-10 10:41:52 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,5 +3,7 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 let ctx = canvas.getContext("2d");
 
-
-ctx.fillRect(0, 0, 100, 100);
+setInterval(function() {
+  ctx.fillStyle = "rgb(200, 200, 200)";
+  ctx.fillRect(Math.random() * 100, Math.random() * 100, 100, 100);
+}, 16);
