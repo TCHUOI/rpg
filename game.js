7d6f5bf0cab82b3a44c01e7754e48e7f142511ec
TCHUOI <tcui@bostonk12.org>
2024-10-21 15:34:02 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,9 +1,6 @@
-function player() {
-    this.x = parseInt(localStorage.getItem("x")) || 0;
-    this.y = parseInt(localStorage.getItem("y")) || 0;
-}
-
 setInterval(function() {
-  ctx.fillStyle = "rgb(200, 200, 200)";
-  ctx.fillRect(Math.random() * 100, Math.random() * 100, 100, 100);
+    ctx.fillStyle = "rgb(200, 200, 200)";
+    ctx.fillRect(Math.random() * 100, Math.random() * 100, 100, 100);
+
+    player.run();
 }, 16);
