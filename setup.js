27d44b56f9da137849a064a83a61a2d1956c52ee
TCHUOI <tcui@bostonk12.org>
2024-10-21 15:32:23 -0400

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,4 +1,41 @@
-function ellipse(x, y, radiusX, radiusY, rotation) {
+let canvas = document.getElementById("canvi");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let ctx = canvas.getContext("2d");
+
+var mouseX = 0;
+var mouseY = 0;
+var dragged = false;
+var clicked = false
+var keys = [];
+
+canvas.addEventListener("mousemove", function(e) {
+    var cRect = canvas.getBoundingClientRect();
+    mouseX = Math.round(e.clientX - cRect.left);
+    mouseY = Math.round(e.clientY - cRect.top);
+});
+canvas.addEventListener("mousedown", function(e) {
+    dragged = true;
+}, false);
+canvas.addEventListener("mouseup", function(e) {
+    if(dragged === true) {
+        clicked = true;
+        dragged = false;
+    }
+}, false);
+window.addEventListener("keydown", keysPressed, false);
+window.addEventListener("keyup", keysReleased, false);
+function keysPressed(e) {
+    // store an entry for every key pressed
+    keys[e.keyCode] = true;
+    e.preventDefault();
+}
+function keysReleased(e) {
+    // mark keys that were released
+    keys[e.keyCode] = false;
+}
+
+ctx.prototype.ellipse = function(x, y, radiusX, radiusY, rotation) {
     ctx.beginPath();
     ctx.ellipse(x, y, radiusX, radiusY, rotation, 0, 2 * Math.PI);
     ctx.fill();
